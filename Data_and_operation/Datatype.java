b399fb5689a1268f676200a394f0145db04ef62d
김강민 <122205332+kkangmen@users.noreply.github.com>
2023-01-21 04:06:54 +0000

    .

--- Data_and_operation/Datatype.java
+++ Data_and_operation/Datatype.java
@@ -2,6 +2,14 @@ package Data_and_operation;
 
 public class Datatype {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(6); //Number
+        System.out.println("six"); //String
+        
+        System.out.println("6"); //String 6
+
+        System.out.println(6+6); //12
+
+        System.out.println("1111".length()); //4
+        System.out.println(1111.length()); // 에러 
     }
 }
