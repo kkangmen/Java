1a8816b3b8791ca9ba1fb7af3be22c35a31cd6f7
김강민 <122205332+kkangmen@users.noreply.github.com>
2023-01-21 03:37:17 +0000

    message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
-import javax.xml.crypto.MarshalException;
-
-public class lifecoding {
+public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
 
 } 
+
+
